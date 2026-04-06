8655e33722a0f6316e52caa9e5dbdb6938114502
Roberto <roberto_hille@estudante.sesisenai.org.br>
2026-04-06 12:56:07 -0300

    add

---
+++ scripts.js
@@ -0,0 +1,23 @@
+document.getElementById("formadocao").addEventListener("submit", function(e){
+    e.preventDefault
+    let valid = true; // Variável que define se todas as informações estão válidas;
+    let nome = document.getElementById("nome").value;
+    let email = document.getElementById("email").value;
+    let numero = document.getElementById("numero").value;
+    let idade = document.getElementById("idade").value;
+    let cpf = document.getElementById("cpf").value;
+    let cidade = document.getElementById("cidade").value;
+    let complemento = document.querySelector('input[name="compl"]:checked');
+    let quintal = document.querySelector('input[name="quint"]:checked');
+    let antes = document.querySelector('input[name="petan"]:checked');
+    let horasozinho = document.getElementById("horas").value;
+    let motivo = document.getElementById("motive").value;
+    let termos = document.getElementById("terms").value;
+
+    if (nome.lenght < 30)
+    {
+        alert("Nome invalido");
+        valid = false;
+    }
+
+})
