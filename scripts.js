3a7033da0f7d69b63280b1fdfa362a723049549b
Roberto <roberto_hille@estudante.sesisenai.org.br>
2026-04-10 11:03:29 -0300

    add

--- scripts.js
+++ scripts.js
@@ -1,5 +1,14 @@
 let counter = 0;
 let EmailValidator = 0;
+const MotivosEstúpidos = [
+    "Porque sim",
+    "AAAAAAAAAA",
+    "Não estou afim",
+    "Foda-se caralho",
+    "pq é fofo"]
+    // Cara eu acho que não existe forma melhor de detectar uma resposta genérica.
+
+
 
 document.getElementById("formadocao").addEventListener("submit", function(e){
     e.preventDefault();
@@ -30,11 +39,18 @@ document.getElementById("formadocao").addEventListener("submit", function(e){
     if (numero.length < 8) return alert("Número Inválido");
     if (idade < 18) return alert("Você precisa de 18 anos ou mais para adotar");
     if (complemento == null) return alert("Informe o complemento");
+    if (complemento.id == "apartamento") return alert("Informe se o apartamento permite animais");
     if (quintal == null) return alert("Informe se tem um quintal");
     if (antes == null) return alert("Informe se já teve um pet antes");
     if (motivo.length < 10) return alert("Motivo muito curto. Insira pelo menos 10 caraceres.");
-    if (termos == null) return alert("CPF Inválido");
-    
-
+    counter = 0;
+    while (counter < MotivosEstúpidos.length)
+    {
+        if (motivo == MotivosEstúpidos[counter] ){
+            alert("Puta que pariu cara da um motivo decente");
+        }
+    counter += 1;
+    }
+    if (termos == null) return alert("Você deve concordar com os termos.");
     document.getElementById("resultado").innerHTML = "Cadastro realizado com sucesso!";
 })
